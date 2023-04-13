c81943f947089bb3dfde37fccdcd5add2267738a
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-04-13 10:31:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(100, -100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(100, 100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(-100, 100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(-100, -100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+basic.showIcon(IconNames.Surprised)
